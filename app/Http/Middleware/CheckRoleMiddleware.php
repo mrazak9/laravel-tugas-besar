df84f375304caf33c5677bd7d193eedd8c2ec068
mrazak9 <muhamadrozak9@gmail.com>
2024-01-16 18:19:20 +0700

    edit roles

---
+++ app/Http/Middleware/CheckRoleMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckRoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle($request, Closure $next, ...$roles)
+    {
+        if (Auth::check() && in_array(Auth::user()->roles, $roles)) {
+            return $next($request);
+        }
+        // Unauthorized, redirect or return response as needed
+        return redirect()->route('home')->with('error', 'Unauthorized access.');
+    }
+}
